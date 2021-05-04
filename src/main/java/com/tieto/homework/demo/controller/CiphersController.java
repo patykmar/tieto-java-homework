c10bc0e12c13591222b39b96f26b75c132535713
patykmar <patyk.m@gmail.com>
2021-05-04 23:00:35 +0200

    added morse handling api

--- src/main/java/com/tieto/homework/demo/controller/CiphersController.java
+++ src/main/java/com/tieto/homework/demo/controller/CiphersController.java
@@ -1,16 +1,20 @@
 package com.tieto.homework.demo.controller;
 
 import com.tieto.homework.demo.repository.CesarRepository;
+import com.tieto.homework.demo.repository.MorseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
-public class CesarController {
+public class CiphersController {
 
     @Autowired
     private CesarRepository cesarRepository;
+
+    @Autowired
+    private MorseRepository morseRepository;
 
 
     @PostMapping("/cesar/crypt")
@@ -21,5 +25,15 @@ public class CesarController {
     @PostMapping("/cesar/decrypt")
     public String postCesarDecryptString(@RequestParam( name = "word", defaultValue = "bgdtc") String word) {
         return this.cesarRepository.decrypt(word);
+    }
+
+    @PostMapping("/morse/crypt")
+    public String postMorseCryptString(@RequestParam( name = "word", defaultValue = "ahoj") String word) {
+        return this.morseRepository.encrypt(word);
+    }
+
+    @PostMapping("/morse/decrypt")
+    public String postMorseDecryptString(@RequestParam( name = "word", defaultValue = ".- .... --- .---") String word) {
+        return this.morseRepository.decrypt(word);
     }
 }
